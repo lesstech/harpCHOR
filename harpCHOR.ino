b595635cb00db444b8c7b2f2821cb5901bb730b0
lesstech <hectic.heat@gmail.com>
2025-09-05 16:19:43 +0200

    code snapshot at presentation

--- harpCHOR.ino
+++ harpCHOR.ino
@@ -3,7 +3,7 @@
 
 //Motor 1
 const int motor1ForwardPin = 13;
-const int motor1BackwardPin = 40;
+const int motor1BackwardPin = 12;
 //Motor 2
 const int motor2ForwardPin = 11;
 const int motor2BackwardPin = 10;
@@ -27,7 +27,7 @@ const int vibro4Pin = 33;
 const int vibro5Pin = 34;
 
 // Servo pins
-const int servo1Pin = 12;
+const int servo1Pin = 45;
 const int servo2Pin = 44;
 
 unsigned long time;
@@ -37,18 +37,18 @@ Servo servo1;
 Servo servo2;
 
 const int vibratePin1 = 5;
-int speed = 180;
+int speed = 100;
 
 const int num_voices = 11;
 int playing[num_voices] {0};
 
 int playing_pins[num_voices] {
-  motor1ForwardPin,
-  motor2ForwardPin,
+  motor5ForwardPin,
   motor3ForwardPin,
   motor4ForwardPin,
-  motor5ForwardPin,
   motor6ForwardPin,
+  motor2ForwardPin,
+  motor1ForwardPin,
   vibro1Pin,
   vibro2Pin,
   vibro3Pin,
@@ -56,9 +56,48 @@ int playing_pins[num_voices] {
   vibro5Pin,
 };
 
+struct motor_speed_range {
+  int min_speed;
+  int max_speed;
+};
+
+motor_speed_range ranges[6] {
+  {20, 180},
+  {100, 180},
+  {100, 180},
+  {100, 180},
+  {100, 180},
+  {100, 180},
+};
+
 struct CustomBaudRate : public MIDI_NAMESPACE::DefaultSettings {
   static const long BaudRate = 38400;
 };
+
+void handleStop() {
+  for (int i = 0; i<num_voices; ++i) {
+    // Handle the vibrators
+    if (i >= 6) {
+      digitalWrite(playing_pins[i], LOW);
+    } else {
+      analogWrite(playing_pins[i], 0);
+    }
+  }
+  servo1.write(135);
+  servo2.write(30);
+  delay(15);
+}
+
+void handleControlChange(byte channel, byte cc_number, byte value) {
+    //servo1.write(135);
+    //servo2.write(30);
+    if (cc_number == 16) {
+      servo1.write(map(value,0,127,135,115));
+    } else if (cc_number == 17) {
+      servo2.write(map(value,0,127,30,55));
+    }
+
+}
 
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
@@ -83,6 +122,12 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
         break;
       case 71:
         playing[6] = velocity;
+        break;
+      case 72:
+        playing[7] = velocity;
+        break;
+      case 74:
+        playing[8] = velocity;
         break;
       default:
         break;
@@ -113,6 +158,12 @@ void handleNoteOff(byte channel, byte pitch, byte velocity)
       case 71:
         playing[6] = -1;
         break;
+      case 72:
+        playing[7] = -1;
+        break;
+      case 74:
+        playing[8] = -1;
+        break;
     }
 
 }
@@ -122,6 +173,8 @@ void setup()
 {
     MIDI.setHandleNoteOn(handleNoteOn);
     MIDI.setHandleNoteOff(handleNoteOff);
+    MIDI.setHandleControlChange(handleControlChange);
+    MIDI.setHandleStop(handleStop);
     MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
 
     //Set pins as outputs
@@ -139,6 +192,8 @@ void setup()
     pinMode(motor6BackwardPin, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(vibro1Pin,OUTPUT);
+    pinMode(vibro2Pin,OUTPUT);
+    pinMode(vibro3Pin,OUTPUT);
 
     for (int i = 0; i<num_voices; ++i)
     {
@@ -147,6 +202,9 @@ void setup()
 
     servo1.attach(servo1Pin);
     servo2.attach(servo2Pin);
+    servo1.write(135);
+    servo2.write(30);
+    delay(15);
 } 
 
 void loop()
@@ -166,15 +224,12 @@ void loop()
         }
       } else {
         if (playing[i] >= 0) {
-          analogWrite(playing_pins[i], speed);
+          analogWrite(playing_pins[i], map(playing[i],0,127,ranges[i].min_speed,ranges[i].max_speed));
         } else {
           analogWrite(playing_pins[i], 0);
         }
       }
     }
 
-    servo1.write(135);
-    delay(20);
-    servo2.write(30);
-    delay(20);
+
 }
